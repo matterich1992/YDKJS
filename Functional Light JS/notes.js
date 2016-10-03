cb0ff5cd8f477e11f591e2e4f0a3a267118846c3
Matthew Erich <matthewerich1992@gmail.com>
2016-10-02 20:28:34 -0700

    Functional Light Programming
    Using functional programming techniques and chaining methods together
    to manipulate lists.

--- Functional Light JS/notes.js
+++ Functional Light JS/notes.js
@@ -184,3 +184,201 @@ mult(3,4,5,6); // --> 360;
 //slice produces hole array to new array
 //splice changes the array in place -- mutable
 
+
+//List Transformation - MAP//
+ function doubleIt(a){
+ 	return a*2;
+ }
+
+function transform(arr,fn){
+	var results = [];
+	for(var i =0;i<arr.length;i++){
+		results[i]= fn(arr[i]);
+	}
+	return results;
+}
+
+transform([1,2,3,4,5], doubleIt); // [2,4,6,8,10]; this array is completely new
+
+// Built in version "prototype" on the array
+
+function doubleIt(a){
+	return a*2;
+}
+
+[1,2,3,4,5].map(doubleIt);
+
+
+// Build filter
+
+function isOdd (a){
+	return a%2===1;
+}
+
+function exlude(arr, fn){
+	var results = [];
+	for(var i=0;i<arr.length;i++){
+		if(fn(arr[i])){
+			results.push(arr[i]);
+		}
+	}
+	return results;
+}
+
+[1,2,3,4,5].filter(isOdd);
+
+//should always return true if you want to keep it
+
+//Compose Values together REDUCE
+
+// idea of summation value a + b = c;
+// Can be used in a 'non-pure' way
+//composition so need 2 values
+
+function mult(x,y){return x*y;} //x = accum; y = val;
+
+function compose(arr, fn, initial){
+	var total = initial;
+	for(var i =0;i<arr.length;i++){
+		total = fn(total,arr[i]);
+	}
+	return total;
+}
+
+compose([1,2,3,4,5], mult, 1);
+
+//accum == you have something like a running total as you continue to multiply
+
+//List Iteration
+
+function logValue(v){console.log(v);}
+
+function iterate(arr, fn){
+	for(avr i=0;i<arr.length;i++){
+		fn(arr[i]);
+	}
+}
+
+
+iterate([1,2,3,4,5], logValue);
+//1,2,3,4,5;
+
+[1,2,3,4,5].forEach(logValue);
+
+//Exercise 4
+//Funcitonal programming Exercise
+
+
+//function foo(){
+//	return 42;
+//}
+
+//function bar() {
+//	return 10;
+//}
+
+
+function foo(x){
+	return function(){
+		return x;
+	};
+}
+
+
+
+
+function add(x,y){ // called a thunk -- function wrapped around a value
+	return x+y;
+}
+
+function add2(fn1,fn2){
+	return add(fn1(), fn2());
+}
+
+//add2(foo, bar); //--> 52
+add2(foo(10), foo(42));
+
+//Exercise 4 Part 2
+
+// Create add(n);
+
+function addn(arr){
+var total = 0;
+for(var i =0;i<arr.length;i++){
+	total = add2(foo(arr[i]), foo(total));
+}
+return total;
+}
+
+
+addn(10,42,56,73);
+
+//Solved with recursion
+// Need a base case- addition of first 2 items plus everything else
+
+/*function addn (arr){
+	if(arr.length<=2) {
+		return add2(foo(arr[0]), foo(arr[1]) );
+
+	}
+	//recursion case
+	return addn([
+		function() {
+			add2(arr[0], arr[1]);
+		}]. concat(arr.slice(2))
+	);
+}*/
+
+
+//Exercise 4 Part 3
+
+function addn(arr){
+	return arr // cuts first value off
+	
+		.reduce(function(prev,curr){
+			return function(){
+				return add2(prev,curr);
+			};
+		},arr[0])();
+}
+
+addn([foo(10), foo(42), foo(56), foo(73)]);
+
+
+
+
+//Exercise 4 part 5
+function addn(arr){
+var total = 0;
+for(var i =0;i<arr.length;i++){
+	total = add2(foo(arr[i]), foo(total));
+}
+return total;
+}
+
+var arr = [10,42,56,73,15,2,98,7];
+
+function isOdd(v){return v%2==1;}
+
+function isEven(x) {return !isOdd(x);}
+
+var isEven = composeNeg(isEven);
+
+arr.filter()
+
+addn(arr.map(foo));
+
+//
+
+
+function isEven(v){return v%2===0;}
+
+var arr = [10,42,56,73,15,2,98,7]
+	.filter(isEven)
+		.map(foo);
+
+addn(...arr); 
+
+
+
+
